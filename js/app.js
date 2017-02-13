8943f48f032f03cf14417a4cc3bae1ef3cbf8bfa
Kunal Singh <278kunal@gmail.com>
2017-02-13 15:28:51 +0530

    Added helpers and converted app to handlebars templating

---
+++ js/app.js
@@ -0,0 +1,48 @@
+(function() {
+
+  registerPartials();
+  renderPage();
+  renderDogs();
+
+  function registerPartials() {
+    Handlebars.registerPartial('dog', $('#dog-template').html());
+  }
+
+  function renderPage() {
+    var template = $('#index-template').html(),
+        compiled = Handlebars.compile(template),
+        rendered = compiled(window.language);
+    $('#main').html(rendered);
+
+    $('#languageSwitch').click(function() {
+      DogPack.switchLanguage();
+    });
+
+    $('#score').find('small').click(function(){
+      DogPack.clearDogs();
+      window.location.href = '?' + Handlebars.helpers.getLanguageFilter(window.language.langId)
+    })
+  }
+
+  function renderDogs() {
+    var template = $('#dogs-template').html(),
+        compiled = Handlebars.compile(template),
+        rendered = compiled({ dogs: DogPack.dogs, language: window.language });
+    $('#theDogs').html(rendered);
+    attachDogButtons();
+  }
+
+  function attachDogButtons() {
+    $('.dog-button').click(function() {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      DogPack.chooseDog(id);
+      renderDogs();
+    });
+
+    $('.not-dog-button').click(function() {
+      var id = $(this).closest('.dog-card').data('dog-id');
+      DogPack.chooseNotDog(id);
+      renderDogs();
+    });
+  }
+})();
