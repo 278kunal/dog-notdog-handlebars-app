83eab55f161abf2fb4d2e88b3cd05c7546c4f2db
Kunal Singh <278kunal@gmail.com>
2017-02-14 16:21:26 +0530

    Sequentialised the gulp tasks for bundling and watching

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,8 @@ var gulp = require('gulp'),
     watch = require('./gulp/watch'),
     templates = require('./gulp/templates');
 
-gulp.task('watch', watch);
-gulp.task('templates', templates);
+gulp.task('templates',templates);
+
+gulp.task('watch',watch);
+
+gulp.task('start', ['templates','watch']);
