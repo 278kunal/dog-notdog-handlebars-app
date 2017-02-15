ca50c110060bd0bd5e98d2e5e9b8159fff517c45
Kunal Singh <Kunal Singh>
2017-02-15 18:14:44 +0530

    changes Server port to default

--- tasks/server.js
+++ tasks/server.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
     exec = require('child_process').exec;
 
 module.exports = function() {
-  exec('node ./node_modules/json-server/bin/index.js --watch app/src/assets/data/db.json --routes app/src/assets/data/routes.json --static ./ --port 443', function (err, stdout, stderr) {
+  exec('node ./node_modules/json-server/bin/index.js --watch app/src/assets/data/db.json --routes app/src/assets/data/routes.json --static ./', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
