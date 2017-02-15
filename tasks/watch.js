33463949c6ce8265f6dcdc7f8c71bafb7b6c1e0e
Kunal Singh <278kunal@gmail.com>
2017-02-15 14:51:47 +0530

    Implmented gulp livereload

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
+    livereload = require('gulp-livereload');
 
 module.exports = function() {
+  livereload.listen();
   gulp.watch('./app/src/templates/*.hbs', ['templates']);
   gulp.watch('./app/src/assets/js/*.js', ['js']);
   gulp.watch('./app/src/assets/css/*.css', ['css']);
